597c183f8886248cfcf6650d135f4531e9d0c5cb
James <canning.jimmy@gmail.com>
2016-09-13 18:33:23 -0700

    Working on user profile

--- imagersite_project/imager_profile/models.py
+++ imagersite_project/imager_profile/models.py
@@ -4,8 +4,9 @@ from django.db.models.signals import post_save
 
 
 class ImagerProfile(models.Models):
+    # this line should link this model to djangos user model and delete the instance of this model
+    # when the link django user moddle is deleted
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     camera_type = models.CharField('camera type', blank=True)
     address = models.CharField('address', blank=True)
     website = models.URLField('website', blank=True)
@@ -17,8 +18,14 @@ class ImagerProfile(models.Models):
         users = User.objects.filter(is_active=True)
         return users.ImagerProfile
 
+    def user_string(self):
+        """a visual representation that appropriately displays when using the Django shell"""
+        user = User.objects.filter(user=self.user)
+        string = "{} shoots {} with a {}".format(user.first_name, user.photography_type, user.camera_type)
+        return string
+
 
 @receiver(post_save, sender=User)
-    def init_new_ImagerProfile:
-        ImagerProfile()
-
+def init_new_ImagerProfile:
+    """initialise a new instance of this class when a new instance of djangos user class is saved"""
+    ImagerProfile()
