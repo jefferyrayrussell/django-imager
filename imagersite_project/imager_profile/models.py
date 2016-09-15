c6429639cfb2c38cf42f51334cb7d3ff3708fccf
James <canning.jimmy@gmail.com>
2016-09-15 08:58:15 -0700

    Imager modele built out

--- imagersite_project/imager_profile/models.py
+++ imagersite_project/imager_profile/models.py
@@ -1,11 +1,19 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+import uuid
 
 
 class ImagerProfile(models.Models):
     # this line should link this model to djangos user model and delete the instance of this model
     # when the link django user moddle is deleted
+    user_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     camera_type = models.CharField('camera type', blank=True)
     address = models.CharField('address', blank=True)
@@ -13,19 +21,25 @@ class ImagerProfile(models.Models):
     photography_type = models.CharField('photography type', blank=True)
     facebook = models.URLField('facebook', blank=True)
 
-    def active():
+    @classmethod
+    def active_users(cls):
         """make a database query for ImagerProfile's that are active in their User moddles"""
-        users = User.objects.filter(is_active=True)
-        return users.ImagerProfile
+        active_users = cls.objects.filter(user__is_active__exact=True)
+        return active_users
 
-    def user_string(self):
+    @property
+    def is_active(self):
+        """Tells us if the current user is active"""
+        return self.user.is_active
+
+    def __str__(self):
         """a visual representation that appropriately displays when using the Django shell"""
         user = User.objects.filter(user=self.user)
         string = "{} shoots {} with a {}".format(user.first_name, user.photography_type, user.camera_type)
         return string
 
 
-@receiver(post_save, sender=User)
-def init_new_ImagerProfile:
+@receiver(models.signals.post_save, sender=User)
+def init_new_imager_profile(sender, **kwargs):
     """initialise a new instance of this class when a new instance of djangos user class is saved"""
-    ImagerProfile()
+    ImagerProfile(user=kwargs['instance']).save()
